a867af5cf0ab44c4c292c7baac6e3a39c7142513
Arman091 <rasoolarman7@gmail.com>
2022-09-21 15:57:11 +0530

    update

---
+++ javascript/total.js
@@ -0,0 +1,24 @@
+
+
+  const arr = []
+    
+  export function toArray(product) {
+
+
+      arr.push(product.price);
+     
+  }
+
+let btn = document.getElementById("check");
+btn.addEventListener("click", () => {
+    let sum = arr.reduce((acc, currv) => {
+        return parseInt(acc, 10) + parseInt(currv, 10)
+    }, 0);
+    
+
+    //=============funtionality to change total==================
+   let price=document.getElementById("label")
+    price.innerText = "$"+sum;
+})
+      
+
